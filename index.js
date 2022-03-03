ec29414d4416e69ba0f72f97ba30d147c3e24773
mby444 <bimayudha161@gmail.com>
2022-03-03 10:15:49 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const http = require("http");
+const express = require("express");
+const ytdl = require("ytdl-core");
+const fs = require("fs");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const getVideos = (formats) => {
+    let videos = formats.filter((format, i) => {
+        return format.hasVideo && format.hasAudio;
+    });
+    videos.reverse();
+    return videos;
+}
+
+app.get("/", async (req, res) => {
+    const vidUrl = req.query.url;
+    if (!vidUrl){
+        return res.render("index", {
+            ready: false
+        });
+    }
+    const vidId = vidUrl.slice(-11);
+    try {
+        const vidInfo = await ytdl.getInfo(vidUrl);
+        const videos = getVideos(vidInfo.formats);
+        res.render("index", {
+            ready: true,
+            vidId,
+            videos
+        });
+    } catch(err){
+        res.render("errors/error", {
+            title: "URL Not Found",
+           errMsg: `URL "${vidUrl}" not found` 
+        });
+    }
+});
+
+app.use("/", (req, res) => {
+    res.status(404);
+    res.send("<h1 style='text-align: center;'>404 Not Found</h1>");
+});
+
+app.listen(port, () => {
+    console.log(`Server running at port ${port}...`)
+});
