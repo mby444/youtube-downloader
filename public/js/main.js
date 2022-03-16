7254be6baf4b92ff1683ef97857924ca35452057
MBY444 <84253932+MBY4444@users.noreply.github.com>
2022-03-16 10:05:16 +0700

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,6 @@
 const contentContainer = document.querySelector(".content-container");
 const form = document.querySelector("form");
 
-// const parseQuery = (query) => {
-//     let queries = query.split("&");
-//     let queryObj = {};
-//     queries.forEach((q, i) => {
-//         let key = q.split("=")[0];
-//         let val = q.split("=")[1];
-//         queryObj[key] = val;
-//     });
-//     return queryObj;
-// }
-
 form.addEventListener("submit", () => {
     contentContainer.innerHTML = "<div class='loader'>loading...</div>";
 });
